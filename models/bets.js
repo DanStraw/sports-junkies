c962ae7addd64647ecbb078bc7947328ec2e6deb
Daniel <danstraw86@gmail.com>
2018-07-10 20:30:43 -0700

    added to bets model

--- models/bets.js
+++ models/bets.js
@@ -6,8 +6,6 @@ const betSchema = new Schema({
     team1: { type: String, required: true },
     team2: { type: String, required: false },
     currentLine: { type: String, required: true },
-    key: { type: String, required: true },
-    
     date: { type: Date, default: Date.now }
   });
   
