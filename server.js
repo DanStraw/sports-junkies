63cb26fc9dfe20713c0a65dd93fe37ad5d9b68bb
Daniel <danstraw86@gmail.com>
2018-08-07 18:03:36 -0700

    added app.get route to prod env

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app.use(passport.session());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client/build'))
+	})
 }
 
 app.get('/home',
